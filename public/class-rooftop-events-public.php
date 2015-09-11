d279f1a9294a478a85ebf7fec149edf745fc9875
Paul Hendrick <paul@errorstudio.co.uk>
2015-09-11 18:07:15 +0100

    generate event post type with full set of labels and attributes

--- public/class-rooftop-events-public.php
+++ public/class-rooftop-events-public.php
@@ -101,11 +101,42 @@ class Rooftop_Events_Public {
 	}
 
     public function register_event_post_type() {
-        register_post_type('event', array(
-            'labels' => array('name' => 'Events', 'singular_name' => 'Event'),
-            'public' => true,
-            'has_archive' => true
-        ));
+        $labels = array(
+            'name'                => _x( 'Events', 'Post Type General Name', 'text_domain' ),
+            'singular_name'       => _x( 'Event', 'Post Type Singular Name', 'text_domain' ),
+            'menu_name'           => __( 'Events', 'text_domain' ),
+            'name_admin_bar'      => __( 'Event', 'text_domain' ),
+            'parent_item_colon'   => __( 'Parent Event:', 'text_domain' ),
+            'all_items'           => __( 'All Event', 'text_domain' ),
+            'add_new_item'        => __( 'Add New Event', 'text_domain' ),
+            'add_new'             => __( 'Add New', 'text_domain' ),
+            'new_item'            => __( 'New Event', 'text_domain' ),
+            'edit_item'           => __( 'Edit Event', 'text_domain' ),
+            'update_item'         => __( 'Update Event', 'text_domain' ),
+            'view_item'           => __( 'View Event', 'text_domain' ),
+            'search_items'        => __( 'Search Event', 'text_domain' ),
+            'not_found'           => __( 'Not found', 'text_domain' ),
+            'not_found_in_trash'  => __( 'Not found in Trash', 'text_domain' ),
+        );
+        $args = array(
+            'label'               => __( 'Event', 'text_domain' ),
+            'description'         => __( 'Event Description', 'text_domain' ),
+            'labels'              => $labels,
+            'supports'            => array( ),
+            'hierarchical'        => false,
+            'public'              => true,
+            'show_ui'             => true,
+            'show_in_menu'        => true,
+            'menu_position'       => 20,
+            'show_in_admin_bar'   => true,
+            'show_in_nav_menus'   => true,
+            'can_export'          => true,
+            'has_archive'         => true,
+            'exclude_from_search' => false,
+            'publicly_queryable'  => true,
+            'capability_type'     => 'page',
+        );
+        register_post_type( 'event', $args );
     }
 
     public function register_event_routes() {
