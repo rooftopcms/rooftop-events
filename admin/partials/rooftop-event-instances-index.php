e26b8e690e0bf5eb3297e1ede9f7208f6c2a7ad0
Paul Hendrick <paul@errorstudio.co.uk>
2016-01-11 17:09:53 +0000

    fetch the 'availability' data from the event_instance_meta data

--- admin/partials/rooftop-event-instances-index.php
+++ admin/partials/rooftop-event-instances-index.php
@@ -14,20 +14,21 @@
         <?php foreach( $event_instances as $row ): ?>
 
             <?php
-            $instance_meta = get_post_meta($row->ID, 'availability', true);
+            $instance_meta = get_post_meta($row->ID, 'event_instance_meta', true);
             $instance_meta = is_array($instance_meta) ? $instance_meta : [];
+            $availability  = @$instance_meta['availability'];
             ?>
 
             <tr data-event-instance-id="<?php echo $row->ID;?>">
                 <td>
                     <a href="?post=<?php echo $row->ID ?>&action=edit">Edit</a>
                 </td>
-                <td><?php echo array_key_exists('starts_at', $instance_meta) ? $instance_meta['starts_at'] : '' ;?></td>
-                <td><?php echo array_key_exists('stops_at', $instance_meta)  ? $instance_meta['stops_at']  : '' ;?></td>
+                <td><?php echo array_key_exists('starts_at', $availability) ? $availability['starts_at'] : '' ;?></td>
+                <td><?php echo array_key_exists('stops_at', $availability)  ? $availability['stops_at']  : '' ;?></td>
                 <td>
-                    <?php if( array_key_exists('seats_available', $instance_meta) ): ?>
-                        <?php echo $instance_meta['seats_available'] ;?>
-                        remaining <span class="hint">(from a total capacity of <?php echo $instance_meta['seats_capacity'] ;?>)</span>
+                    <?php if( array_key_exists('seats_available', $availability) ): ?>
+                        <?php echo $availability['seats_available'] ;?>
+                        remaining <span class="hint">(from a total capacity of <?php echo $availability['seats_capacity'] ;?>)</span>
                     <?php endif;?>
                 </td>
             </tr>
