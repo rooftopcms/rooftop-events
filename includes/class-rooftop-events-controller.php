cce99265697504d00d5fece1f96ce457bfdda500
Paul Hendrick <paul@errorstudio.co.uk>
2016-01-19 12:11:12 +0000

    when updating an event via the API, check if the updated metadata includes a 'genre' key and add it to the post meta as event_genre

--- includes/class-rooftop-events-controller.php
+++ includes/class-rooftop-events-controller.php
@@ -8,6 +8,16 @@ class Rooftop_Events_Controller extends Rooftop_Controller {
         add_action( "rooftop_".$this->post_type."_rest_insert_post", function( $prepared_post, $request, $success ){
             update_post_meta( $prepared_post->ID, 'event_id', $request['event_id'] );
 
+            $updated_meta = $request[$this->post_type."_meta"];
+
+            // we need the event genre to be a top-level attribute (not a part of the event_meta array)
+            // so that we can easily query related events that value
+            if( array_key_exists( 'genre', $updated_meta ) && strlen( $updated_meta['genre'] ) ) {
+                update_post_meta( $request['id'], 'event_genre', $updated_meta['genre'] );
+            }else {
+                delete_post_meta( $request['id'], 'event_genre' );
+            }
+
             return $prepared_post;
         }, 10, 3);
     }
