a7d1c5e03ec2c7e3c383d8e659bbf5ac3470383f
Paul Hendrick <paul@errorstudio.co.uk>
2016-05-26 15:42:43 +0100

    remove old cruft

--- includes/class-rooftop-events.php
+++ includes/class-rooftop-events.php
@@ -168,10 +168,6 @@ class Rooftop_Events {
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_styles' );
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts' );
 
-        $this->loader->add_action( 'init', $plugin_admin, 'create_tables' );
-        $this->loader->add_action( 'wpmu_new_blog', $plugin_admin, 'create_tables' );
-        $this->loader->add_action( 'delete_blog', $plugin_admin, 'remove_tables', 20 );
-
         /**
          *
          */
